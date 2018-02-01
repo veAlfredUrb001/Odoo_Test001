3bcce718a4c02df3c92bf88e31c55e7ab4617bc9
veAlfredUrb001 <luis.vil.urb@gmail.com>
2018-02-01 12:10:37 -0400

    180201_1210 => final99

--- addons/openacademy/__manifest__.py
+++ addons/openacademy/__manifest__.py
@@ -1,29 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "openacademy",
-
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+    'name': "OpenAcademy",
+    'version': '0.1',
+    'category': 'Extra Tools',    
+    'summary': 'Manage Online Courses and Trainnings',
     'description': """
-        Long description of module's purpose
-    """,
+   Manage and Sell of Courses and Trainnings
+==========================================
 
+ Central management of Courses and Trainnings.
+    """,
     'author': "LAVU Corp",
     'website': "http://www.LAVUCopr.com",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
     # 03:39 01/02/2018 LAV => 'depends': ['base'],
     'depends': ['base', 'board'],
-
-    # always loaded
     'data': [
         'security/security.xml',
         'security/ir.model.access.csv',
@@ -34,7 +24,7 @@
         'views/session_board.xml',
         'rpt/reports.xml',
     ],
-    # only loaded in demonstration mode
+ 
     'demo': [
         'demo/demo.xml',
     ],
